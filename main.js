2167cbb1b8a1c304cba0f7d06e59c1430463e820
robloxmuskateers <79553090+robloxmuskateers@users.noreply.github.com>
2021-09-30 17:57:27 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,39 @@
+prediction_1 = "blank";
+prediction_2 = "blank";
+
+Webcam.set({
+    width: 350,
+    height: 300,
+    image_format: 'png',
+    png_quality: 90
+});
+
+camera = document.getElementById("camera");
+Webcam.attach('#camera');
+
+function takeSnapshot() {
+    Webcam.snap(function(data_uri) {
+        document.getElementById("result").innerHTML = '<img id="captured_image" src="' + data_uri + '">';
+    })
+}
+
+console.log('ml5 version: ' + ml5.version);
+
+classifier = ml5.imageClassifier('https://storage.googleapis.com/tm-model/EAphvEHcZ/model.json', modelLoaded);
+
+function modelLoaded() {
+    console.log("model loaded!");
+    speak();
+}
+
+function speak() {
+    synth = window.speechSynthesis;
+    speak_data_1 = "The first prediction is " + prediction_1;
+    speak_data_2 = "And the second prediction is " + prediction_2;
+    var utterThis = new SpeechSynthesisUtterance(speak_data_1 + speak_data_2);
+    synth.speak(utterThis);
+}
+
+function check() {
+    console.log("check");
+}
